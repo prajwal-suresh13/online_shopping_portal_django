75b99089d3c560368eda5f844b9acfc3b355fe13
Prajwal Suresh <113749686+prajwal-suresh13@users.noreply.github.com>
2022-09-26 23:56:01 +0530

    Update settings.py

--- project/shopping/settings.py
+++ project/shopping/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4slhq+gel@t+^@mtrq(9noema@#x83pz9bt)mhu9aye+-&p39g'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
